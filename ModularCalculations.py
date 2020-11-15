ab09f349ca6418f21fef5a726f5afa31de8a9a55
Manuel Carvalho <burrowsrd@gmail.com>
2020-11-15 13:29:28 +1100

    Added ModularCalculations.py (2)

--- ModularCalculations.py
+++ ModularCalculations.py
@@ -6,6 +6,22 @@ print(val)
 val = (4*5) % 12
 print(val)
 
-g = 2
-for i in range(20):
-    print(i, (g**i) % 5)
+# g = 2
+# for i in range(7):
+#     print(i, g ** i % 5)
+
+g = 3
+
+min_gen = 1000
+
+for i in range(7):
+
+    val_gen = (g**i) % 7
+
+    if min_gen > val_gen:
+
+        min_gen = val_gen
+
+    print(i,val_gen)
+
+print(min_gen)
